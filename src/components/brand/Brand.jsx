9df587638c263ff2b1a8ad83309a178dad489f4d
jeffrey <pengtuzhang@gmail.com>
2021-11-18 10:14:13 +1100

    finished feature section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './imports';
 
 const Brand = () => {
     return (
-        <div>
-            
+        <div className="gpt3__brand section__padding">
+            <div>
+                <img alt="google" src={google}/>
+            </div>
+            <div>
+                <img alt="slack" src={slack}/>
+            </div>
+            <div>
+                <img alt="atlassian" src={atlassian}/>
+            </div>
+            <div>
+                <img alt="dropbox" src={dropbox}/>
+            </div>
+            <div>
+                <img alt="shopify" src={shopify}/>
+            </div>
         </div>
     )
 }
