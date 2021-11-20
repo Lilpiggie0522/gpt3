2737b25a0a57795b94ab140816a3082b8558eeb9
jeffrey <pengtuzhang@gmail.com>
2021-11-20 12:38:38 +1100

    changed header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <div className="gpt3__header section__padding" id="Home">
             <div className="gpt3__header-content">
-                <h1 className="gradient__text">Let's Build Something amazing with GPT-3 OpenAI</h1>
+                <h1 className="gradient__text">你这个肥猪你，看看我网站做的怎么样？</h1>
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
                 <div className="gpt3__header-content__input">
